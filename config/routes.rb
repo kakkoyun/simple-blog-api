d4b5c2a909fb7cef3fd35d20bfc51e4d5310cc37
Kemal Akkoyun <kakkoyun@gmail.com>
2016-07-24 16:31:05 +0300

    Add routes for users resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
+# == Route Map
+#
+#       Prefix Verb   URI Pattern                 Controller#Action
+# api_v1_users GET    /api/v1/users(.:format)     api/v1/users#index
+#              POST   /api/v1/users(.:format)     api/v1/users#create
+#  api_v1_user GET    /api/v1/users/:id(.:format) api/v1/users#show
+#              PATCH  /api/v1/users/:id(.:format) api/v1/users#update
+#              PUT    /api/v1/users/:id(.:format) api/v1/users#update
+#              DELETE /api/v1/users/:id(.:format) api/v1/users#destroy
+#
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
