b4b81fef64e610d0bb8c504f83c0320f50da9b77
Kemal Akkoyun <kakkoyun@gmail.com>
2016-07-24 14:30:57 +0300

    Configure basic caching.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,10 +4,10 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes               = false
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load                  = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -16,9 +16,9 @@ Rails.application.configure do
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
-    config.cache_store = :memory_store
+    config.cache_store                = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
+        'Cache-Control' => 'public, max-age=172800'
     }
   else
     config.action_controller.perform_caching = false
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  config.active_support.deprecation    = :log
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
@@ -38,5 +38,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher                  = ActiveSupport::EventedFileUpdateChecker
 end
